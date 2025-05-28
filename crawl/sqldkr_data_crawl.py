74a758f1af883839e9b1c948223cb4bf1ae9605f
Apic <dlsdud9098@naver.com>
2025-05-28 18:06:34 +0900

    sqld.kr 크롤링

--- crawl/sqldkr_data_crawl.py
+++ crawl/sqldkr_data_crawl.py
@@ -1,80 +1,32 @@
-# import asyncio
-# from playwright.async_api import async_playwright
-# import pandas as pd
-# import random
-# from itertools import chain
-# from tqdm import tqdm
-
-# # 리스트 나누기
-# def split_url(urls, split_num):
-#     new_urls = []   
-#     for i in range(0, len(urls), split_num):
-#         new_urls.append(urls[i: i+split_num])
-
-#     return new_urls
-
-# async def fetch_table(playwright, idx, url):
-#     browser = await playwright.chromium.launch(headless=False)
-#     context = await browser.new_context()
-#     page = await context.new_page()
-    
-#     try:
-#         # print(f"[{idx}] 접속 중: {url}")
-#         await page.goto(url, wait_until='domcontentloaded')
-#         await page.wait_for_selector('#frm', timeout=10000)  # 최대 10초 대기
-
-#         page.locator('body > div.login-container > button').click()
-        
-#         await asyncio.sleep(10)
-
-#     except Exception as e:
-#         print(f"[{idx}] 에러 발생: {e}")
-#     finally:
-#         await context.close()
-#         await browser.close()
-
-# async def main():
-#     urls = ["https://www.sqld.kr/"] * 5
-    
-#     urls = urls[-1:]
-#     # print(urls)
-#     # urls = split_url(urls, 5)
-    
-#     results = []
-#     async with async_playwright() as playwright:
-#         for url_list in tqdm(urls, total=len(urls)):
-#             tasks = [fetch_table(playwright, idx, url) for idx, url in enumerate(url_list)]
-#             batch_results = await asyncio.gather(*tasks)
-#             # results.extend(batch_results)
-
-#             # await asyncio.sleep(random.randint(3, 7))
-
-#     # print(results)
-#     # results는 리스트 안에 리스트가 있으니 평탄화
-#     # flat_results = list(chain.from_iterable(results))
-#     # df = pd.DataFrame(flat_results)
-#     # df.to_csv('sqld_kr_data.csv', encoding='utf-8', index=False)
-
-# asyncio.run(main())
-
-
 import asyncio
 from playwright.async_api import async_playwright
-import random
 import pandas as pd
+import random
+from itertools import chain
+from tqdm import tqdm
+import re
+
+# 리스트 나누기
+def split_url(urls, split_num):
+    new_urls = []   
+    for i in range(0, len(urls), split_num):
+        new_urls.append(urls[i: i+split_num])
+
+    return new_urls
 
 async def fetch_table(playwright, idx, url):
+    # url, p_index = arg
     browser = await playwright.chromium.launch(
-        headless=False,
+        headless=True,
         args= ['--disable-blink-features=AutomationControlled']
         )
     context = await browser.new_context()
     page = await context.new_page()
 
+    # print(url)
     try:
         # print(f"[{idx}] 접속 중: {url}")
         await page.goto(url, wait_until='domcontentloaded')
-        # await page.wait_for_selector('#frm', timeout=10000)  # 최대 10초 대기
 
         await page.locator('body > div.login-container > button').click()
         
@@ -95,10 +47,19 @@ async def fetch_table(playwright, idx, url):
         await page.wait_for_selector('#mock-exam-button')
         await page.locator('#mock-exam-button').click()
 
+        await page.wait_for_selector('#content-container > div.question-content')
+        match = re.search(r'/mock-exam/(\d+)/', page.url)
+        if match:
+            page_id = match.group(1)
+            
         datas = []
         for idx in range(50):
+            current_url = f'https://www.sqld.kr/mock-exam/{page_id}/question/{idx}?is_review=True'
+            await page.goto(current_url, wait_until='domcontentloaded')
+
             # 태그 생길때 까지 기다리기
             await page.wait_for_selector('#content-container > div.question-content')
+
             # 문제 제목
             question_title = await page.locator('#content-container > div.question-content').inner_text()
             question_title = question_title.replace('[문제]', '').strip()
@@ -132,18 +93,12 @@ async def fetch_table(playwright, idx, url):
                 choice_text = choice_text.replace('\t', '').replace('\n', '')[1:]
                 choices.append(choice_text)
 
-            # 번호 선택
-            await page.locator('#option-1').click()
-            # 다음 문제
-            
-            # 버튼 로딩 기다리기
-            await page.wait_for_selector('#next-question')
-            await page.locator('#next-question').click()
-            # await page.locator('#options-container > form > div.mt-4.text-center > div').click()
-            # print('다음 버튼 클릭')
+            # 정답
+            answer1 = await page.locator('#options-container > form > div.mt-4.text-center > div > div:nth-child(2)').inner_text()
+            answer2 = await page.locator('#options-container > form > div.mt-4.text-center > div > div:nth-child(3)').inner_text()
+            answer = '\n'.join([answer1.strip(), answer2.strip()])
 
-            answer = None
-            # print(question_title, text_part, choices)
+            # print(question_title, text_part, choices, answer)
             data = {
                 'id': idx+1,
                 'title': question_title,
@@ -157,22 +112,37 @@ async def fetch_table(playwright, idx, url):
             }
             datas.append(data)
 
+            await asyncio.sleep(.5)
+
         await asyncio.sleep(random.randint(3, 7))
         return datas
 
     except Exception as e:
         print(f"[{idx}] 에러 발생: {e}")
     finally:
+        
         await context.close()
         await browser.close()
 
 async def main():
-    url = "https://www.sqld.kr/"
+    urls = ["https://www.sqld.kr/"] * 1000
     
+    # urls = urls[-1:]
+    # print(urls)
+    urls = split_url(urls, 5)
+    
+    page_index = range(8000, 6000)
+    
+    results = []
     async with async_playwright() as playwright:
-        result = await fetch_table(playwright, 0, url)
+        for url_list in tqdm(urls, total=len(urls)):
+            tasks = [fetch_table(playwright, idx, url) for idx, url in enumerate(url_list)]
+            batch_results = await asyncio.gather(*tasks)
+            results.extend(batch_results)
 
-    df = pd.DataFrame(result)
-    df.to_csv('data.csv', encoding='utf-8', index=False)
+    # results는 리스트 안에 리스트가 있으니 평탄화
+    flat_results = list(chain.from_iterable(results))
+    df = pd.DataFrame(flat_results)
+    df.to_csv('data/sqld_kr_data.csv', encoding='utf-8', index=False)
 
 asyncio.run(main())
