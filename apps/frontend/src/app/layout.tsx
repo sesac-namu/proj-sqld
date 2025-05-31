30a3ecd0418a9ad97f285c6112010ef98772b177
BoRamSon <onbrson1@gmail.com>
2025-05-31 22:38:24 +0900

    fix: 화면 전체 채우기

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,3 +1,5 @@
+// src/app/layout.tsx
+
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
@@ -29,7 +31,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AuthProvider>
           <Header />
-          <main className='container mx-auto'>{children}</main>
+          {children}
         </AuthProvider>
       </body>
     </html>
--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,16 +1,27 @@
 // src/app/page.tsx
+import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-br from-slate-900 to-slate-700 text-white'>
+    <main
+      className='flex min-h-screen flex-col items-center justify-center 
+                  p-24 bg-gradient-to-br from-slate-900 to-slate-700 text-white'
+    >
       <div className='text-center'>
-        <h1 className='text-5xl font-bold mb-8 animate-pulse'>SQLD CBT</h1>
+        <h1 className='text-5xl font-bold mb-8'>SQLD CBT</h1>
         <p className='text-xl mb-12'>
           SQLD 자격증 합격을 위한 최고의 연습 플랫폼에 오신 것을 환영합니다!
         </p>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg text-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105'>
-          학습 시작하기
-        </button>
+        <Link href='/quiz'>
+          <button
+            className='bg-blue-500 hover:bg-blue-700 text-white 
+                        font-bold py-3 px-6 rounded-lg text-lg shadow-md 
+                        hover:shadow-lg transition duration-300 ease-in-out 
+                        transform hover:scale-105 animate-pulse'
+          >
+            학습 바로가기
+          </button>
+        </Link>
       </div>
     </main>
   );
